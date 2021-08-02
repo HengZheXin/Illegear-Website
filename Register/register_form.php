58bf089ec954c33eeb2e731235d5019d94908d1a
CCC-8 <86913260+CCC-8@users.noreply.github.com>
2021-08-03 00:06:09 +0800

    idk what have i done kill me please

--- Register/register_form.php
+++ Register/register_form.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" href="register.css">
-        <title></title>
+        <title>Register | IGS Website</title>
         <?php require '../ShareDesign/styles.php'; ?><!--css links. file found in utils folder-->
         <?php require '../ShareDesign/scripts.php'; ?><!--js links. file found in utils folder-->
     </head>
