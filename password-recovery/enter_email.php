d763962bd8d8def41255b846e8f75f92027acfe0
CCC-8 <86913260+CCC-8@users.noreply.github.com>
2021-08-17 21:20:26 +0800

    what have i done

--- password-recovery/enter_email.php
+++ password-recovery/enter_email.php
@@ -4,21 +4,24 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Password Reset PHP</title>
+	<title>Reset Password</title>
 	<link rel="stylesheet" href="main.css">
 </head>
+
 <body>
+	<div class="container">
 	<form class="login-form" action="enter_email.php" method="post">
 		<h2 class="form-title">Reset password</h2>
 		<!-- form validation messages -->
 		<?php include('messages.php'); ?>
-		<div class="form-group">
-			<label>Your email address</label>
-			<input type="email" name="email">
+		<div class="input-box">
+			<input type="email" placeholder="Enter your email address" required>
+
 		</div>
 		<div class="form-group">
 			<button type="submit" name="reset-password" class="login-btn">Submit</button>
 		</div>
 	</form>
+	</div>
 </body>
 </html>
