04eba75e51e851632fcb6973975fe18ccf508fda
CCC-8 <86913260+CCC-8@users.noreply.github.com>
2021-09-06 18:02:56 +0800

    updated 3

--- Home/home.php
+++ Home/home.php
@@ -226,7 +226,7 @@
 
     <section class="section6">
         <div class="leftpic">
-            <img src="images/bottomlogo.png" alt="Logo">
+            <img src="images/bottomlogo.png" alt="Logo" style="margin-left: 20px;">
         </div>
         <div class="righttext">
             <h1>JOIN OUR SOCIETY</h1><br><br>
