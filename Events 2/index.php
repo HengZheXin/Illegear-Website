6ecd89d239ad41b1d63e6eb56392877e2e7f2aaa
ZX <hengzhexin@gmail.com>
2021-09-10 00:41:49 +0800

    DONE

--- Events 2/index.php
+++ Events 2/index.php
@@ -16,51 +16,6 @@
 </head>
 <body>
 
-<!--
-<php
-
-$con = new PDO("mysql:host=localhost;dbname=igswebdb",'root','');
-
-if (isset($_POST["submit"])) {
-	$str = $_POST["search"];
-	$sth = $con->prepare("SELECT * FROM `event` WHERE Name = '$str'");
-
-	$sth->setFetchMode(PDO:: FETCH_OBJ);
-	$sth -> execute();
-
-	if($row = $sth->fetch())
-	{
-		?>
-		<br><br><br>
-		<table>
-			<tr>
-				<th>Name</th>
-				<th>Description</th>
-			</tr>
-			<tr>
-				<td><php echo $row->EventName; ?></td>
-			</tr>
-
-		</table>
-<php 
-	}
-		
-		
-		else{
-			echo "Name Does not exist";
-		}
-
-
-}
-
-?>
-
-
--->
-
-
-
-
     <?php require_once '../ShareDesign/header2.php'; ?>
 
     <div class="container5">
