ed09f461e1928154c66a89ac273d13a74570965f
ZX <hengzhexin@gmail.com>
2022-03-29 13:59:17 +0800

    updated footer

--- Products/cart.php
+++ Products/cart.php
@@ -110,45 +110,7 @@
 
 <!----------Footer---------------> 
 
-<div class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="footer-col1">
-               <h3>Download Our App</h3>
-                <p>Download App for Android and ios mobile phone.</p>
-                <div class="app-logo">
-                    <img src="images/play-store.png">
-                    <img src="images/app-store.png">
-                </div>
-            </div>
-            <div class="footer-col2">
-               <img src="images/logo-white.png">
-                <p>Our Purpose Is To Sustainably Make the Pleasure and Benefits of Sports Accessible to the Many.</p>
-            </div>
-            <div class="footer-col3">
-               <h3>Useful Links</h3>
-                <ul>
-                    <li>Coupons</li>
-                    <li>Blog Post</li>
-                    <li>Return Policy</li>
-                    <li>Join Affiliate</li>
-                </ul>
-            </div>
-            <div class="footer-col4">
-               <h3>Follow Us</h3>
-                <ul>
-                    <li>Facebook</li>
-                    <li>Twitter<li>
-                    <li>Instagram</li>
-                    <li>YouTube</li>
-                </ul>
-            </div>
-        </div>
-        <hr>
-        <p class="copyright"><a href="https://www.youtube.com/c/EasyTutorialsVideo?sub_confirmation=1">Copyright 2020 - Easy Tutorials</a></p>
-    </div>
-    
-</div>
+<?php require_once '../ShareDesign/footer.php'; ?>
 
 
 
